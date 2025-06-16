aa44f0c12b66fee7366c4d292425103fe7d10f40
RosaAnderson <admin@andersonrosa.com.br>
2025-06-16 14:29:14 -0300

    Algorítimos e Lógica de Programação

---
+++ 1º Semestre/Algorítimos e Lógica de Programação/Exercícios/other/tProg0005.cpp
@@ -0,0 +1,22 @@
+#include "stdlib.h"
+# include "stdio.h"
+
+int  main()
+{
+	int var = 666;
+	
+	printf("informe um numero: ");
+	
+	
+	scanf("%i", &var);
+	system("cls");
+	
+	printf("o valor de var eh: %i" , var);
+	
+	
+
+	return 0;
+}
+
+
+
