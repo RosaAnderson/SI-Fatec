4abd0fbdfac0814384c8386257c9c67567b07973
RosaAnderson <admin@andersonrosa.com.br>
2025-06-16 14:37:09 -0300

    Algorítimos e Lógica de Programação

---
+++ 1º Semestre/Algorítimos e Lógica de Programação/Exercícios/program/Aula/_diversos Curtição/d_046_of_100.cpp
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "locale.h"
+#include "time.h"
+#include "string.h"
+
+/**
+ * 
+/**/
+
+int main()
+{
+
+    system("pause");
+
+    return 5500;
+}
