4dac0e819bb1b7b793a02be2e7a0b331a09b020f
RosaAnderson <admin@andersonrosa.com.br>
2025-06-16 15:01:17 -0300

    Arquitetura Orientada a Serviços

---
+++ 6º Semestre/Arquitetura Orientada a Serviços/Exercícios/livros 2025-06-05/editora/bd/editora.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23-Maio-2025 às 00:26
+-- Versão do servidor: 10.4.21-MariaDB
+-- versão do PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `editora`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `autores`
+--
+
+CREATE TABLE `autores` (
+  `id_autores` int(10) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `nacionalidade` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `autores`
+--
+
+INSERT INTO `autores` (`id_autores`, `nome`, `nacionalidade`) VALUES
+(1, 'Autor 1', 'Brasileira'),
+(2, 'Autor 2', 'Inglês');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `generos`
+--
+
+CREATE TABLE `generos` (
+  `id_generos` int(10) NOT NULL,
+  `descritivo` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `generos`
+--
+
+INSERT INTO `generos` (`id_generos`, `descritivo`) VALUES
+(1, 'Romance'),
+(2, 'Suspense');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `id_livros` int(10) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `sinopse` text NOT NULL,
+  `preco` float NOT NULL,
+  `id_generos` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `livros`
+--
+
+INSERT INTO `livros` (`id_livros`, `titulo`, `sinopse`, `preco`, `id_generos`) VALUES
+(1, 'Livro 1', 'Livro 1', 100, 1),
+(2, 'Livro 2', 'Livro 2', 60, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros_autores`
+--
+
+CREATE TABLE `livros_autores` (
+  `id_livros_autores` int(10) NOT NULL,
+  `id_livros` int(10) NOT NULL,
+  `id_autores` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `livros_autores`
+--
+
+INSERT INTO `livros_autores` (`id_livros_autores`, `id_livros`, `id_autores`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 2);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `autores`
+--
+ALTER TABLE `autores`
+  ADD PRIMARY KEY (`id_autores`);
+
+--
+-- Índices para tabela `generos`
+--
+ALTER TABLE `generos`
+  ADD PRIMARY KEY (`id_generos`);
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`id_livros`),
+  ADD KEY `id_generos` (`id_generos`);
+
+--
+-- Índices para tabela `livros_autores`
+--
+ALTER TABLE `livros_autores`
+  ADD PRIMARY KEY (`id_livros_autores`),
+  ADD KEY `id_livros` (`id_livros`,`id_autores`),
+  ADD KEY `id_autores` (`id_autores`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `autores`
+--
+ALTER TABLE `autores`
+  MODIFY `id_autores` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `generos`
+--
+ALTER TABLE `generos`
+  MODIFY `id_generos` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `id_livros` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `livros_autores`
+--
+ALTER TABLE `livros_autores`
+  MODIFY `id_livros_autores` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD CONSTRAINT `livros_ibfk_1` FOREIGN KEY (`id_generos`) REFERENCES `generos` (`id_generos`);
+
+--
+-- Limitadores para a tabela `livros_autores`
+--
+ALTER TABLE `livros_autores`
+  ADD CONSTRAINT `livros_autores_ibfk_1` FOREIGN KEY (`id_livros`) REFERENCES `livros` (`id_livros`),
+  ADD CONSTRAINT `livros_autores_ibfk_2` FOREIGN KEY (`id_autores`) REFERENCES `autores` (`id_autores`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
